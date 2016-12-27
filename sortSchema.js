d5e3a5924191af1e4337fa4fe0c622e7ccf31859
David Hérault <dherault@gmail.com>
2016-12-27 17:08:40 +0100

    pass tests

--- sortSchema.js
+++ sortSchema.js
@@ -1,5 +1,33 @@
-function sortSchema(schemaObject) {
+function sortSchema(schemaPart) {
+  if (typeof schemaPart === 'object') {
 
+    if (schemaPart === null) return schemaPart;
+
+    if (Array.isArray(schemaPart)) {
+      const strings = [];
+      const objects = [];
+
+      schemaPart.forEach(part => {
+        const type = typeof part;
+
+        if (type === 'object') objects.push(sortSchema(part));
+        else if (type === 'string') strings.push(part);
+        else throw new Error(`Unknow type: ${type}`);
+      });
+
+      return strings.sort().concat(objects.sort((a, b) => a.name < b.name ? -1 : 1));
+    }
+
+    const newObject = {};
+
+    Object.keys(schemaPart)
+    .sort()
+    .forEach(key => newObject[key] = sortSchema(schemaPart[key]));
+
+    return newObject;
+  }
+
+  return schemaPart;
 }
 
 module.exports = sortSchema;
--- test/index.js
+++ test/index.js
@@ -1,11 +1,72 @@
 /* global describe it */
 const { assert } = require('chai');
 const sortSchema = require('../sortSchema');
+const input = require('./input.json');
+
+function findKeysAndNames(obj, keys = [], names = []) {
+  if (obj && typeof obj === 'object') {
+    Object.keys(obj).forEach(key => {
+      keys.push(key);
+
+      if (key === 'name') names.push(obj[key]);
+
+      findKeysAndNames(obj[key], keys, names);
+    });
+  }
+
+  return { keys, names };
+}
+
+function deepRandomize(obj) {
+  if (!obj || typeof obj !== 'object') return obj;
+
+  if (Array.isArray(obj)) {
+    const newArray = [];
+    const keys = Object.keys(obj);
+
+    while (keys.length) {
+      const key = keys.splice(Math.floor(Math.random() * keys.length), 1);
+
+      newArray.push(deepRandomize(obj[key]));
+    }
+
+    return newArray;
+  }
+
+  const newObject = {};
+  const keys = Object.keys(obj);
+
+  while (keys.length) {
+    const key = keys.splice(Math.floor(Math.random() * keys.length), 1);
+
+    newObject[key] = deepRandomize(obj[key]);
+  }
+
+  return newObject;
+}
 
 describe('Sorting', () => {
 
-  it('sorts a GraphQL schema', () => {
+  it('is deterministic', () => {
+    assert.deepEqual(sortSchema(input), sortSchema(input));
+  });
 
+  it('deeply sorts objects', () => {
+    assert.deepEqual(sortSchema(deepRandomize(input)), sortSchema(input));
+  });
+
+  it('returns the same schema', () => {
+    const {
+      keys: expectedKeys,
+      names: expectedNames,
+    } = findKeysAndNames(input);
+    const {
+      keys: actualKeys,
+      names: actualNames,
+    } = findKeysAndNames(sortSchema(input));
+
+    assert.sameMembers(actualKeys, expectedKeys);
+    assert.sameMembers(actualNames, expectedNames);
   });
 
 });
