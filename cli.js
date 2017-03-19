cb891f76047a8bba300c8300718ba09b31b80b08
David Hérault <dherault@gmail.com>
2017-03-19 13:02:51 +0100

    lint and add contributors

--- cli.js
+++ cli.js
@@ -7,18 +7,17 @@ const sortSchema = require('./sortSchema');
 
 const files = process.argv.slice(2);
 
-files.forEach(sortFile);
+files.forEach(fileName => {
+  const filePath = path.resolve(fileName);
 
-function sortFile(fileName) {
-    const filePath = path.resolve(fileName);
-    try {
-        const fileData = fs.readFileSync(filePath, 'utf8');
-        const json = JSON.parse(fileData);
-        const sortedSchema = sortSchema(json);
-        fs.writeFile(filePath, JSON.stringify(sortedSchema, null, '  '));
-    }
-    catch (e) {
-        console.error('failed convert ' + fileName);
-        console.error(err);
-    }
-}
+  try {
+    const fileData = fs.readFileSync(filePath, 'utf8');
+    const json = JSON.parse(fileData);
+    const sortedSchema = sortSchema(json);
+    fs.writeFile(filePath, JSON.stringify(sortedSchema, null, 2));
+  }
+  catch (err) {
+    console.error(`Failed to sort ${fileName}`);
+    console.error(err);
+  }
+});
